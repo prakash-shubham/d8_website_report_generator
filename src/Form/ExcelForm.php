2c408fabf33982095c2a06696615096e02527e60
prakash-shubham <prakash_shubham@ymail.com>
2019-04-25 19:23:54 +0530

    Added Excel Generator

--- src/Form/ExcelForm.php
+++ src/Form/ExcelForm.php
@@ -33,13 +33,46 @@ class ExcelForm extends FormBase {
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
-  
-    $report = \Drupal::service('d8_website_report_generator.ReportGenerator')->report_generator();
 
-   drupal_set_message(print_r($report, true));
+  	$types = \Drupal::entityTypeManager()
+      ->getStorage('node_type')
+      ->loadMultiple();
+    $options = [];
+
+    // file name for download
+    $fileName = "codexworld_export_data" . date('Ymd') . ".xls";
+
+    // headers for download
+    header("Content-Disposition: attachment; filename=\"$fileName\"");
+    header("Content-Type: application/vnd.ms-excel");
+
+    $flag = false;
+
+    foreach ($types as $node_type) {
+      foreach (\Drupal::entityManager()->getFieldDefinitions('node', $node_type->id() ) as $field_name   => $field_definition) {
+        if (!empty($field_definition->getTargetBundle())) {
+
+          $data =  array(array("Content Type" => $node_type->label(), "Field Name" => $field_name, "Field Type" => $field_definition->getType(), "Message" => "none"));
+
+          foreach($data as $row) {
+       
+            if(!$flag) {
+              // display column names as first row
+        
+              echo implode("\t", array_keys($row)) . "\n";
+              $flag = true;
+            }
+
+            // filter data
+            array_walk($row, 'filterData');
+            echo implode("\t", array_values($row)) . "\n";
+          }
+        }
+      }
+    }   
     
+    exit;
+
   }
-
 }
-
 
